957a5a3362346ba38bc7b0a4d1793e55d6641846
gouranga <gourangaroy831@gmail.com>
2026-03-03 00:06:37 +0600

    Loan system and total amount

--- operator.c
+++ operator.c
@@ -172,7 +172,7 @@ for(n = 1; n <= 20; n++) {
         }
     }
     */
-
+/*
    int a, b, c;
 
     for(a = 1; a <= 3; a++) {
@@ -184,7 +184,88 @@ for(n = 1; n <= 20; n++) {
                 }
         }
     }
+*/
+/*
+    double x, y, x_plus_y, x_minus_y;
 
+    printf("Enter the value of x + y : ");
+    scanf("%lf", &x_plus_y);
+
+    printf("Enter the value of x - y : ");
+    scanf("%lf", &x_minus_y);
+
+    x = (x_plus_y + x_minus_y) / 2;
+    y = (x_plus_y - x_minus_y) / 2;
+
+    printf("x = %0.2lf, y = %0.2lf\n", x, y );
+
+    */
+
+    /*
+    double a1, a2, b1, b2, c1, c2, x, y;
+
+    printf("a1 = ");
+    scanf("%lf", &a1);
+    printf("a2 = ");
+    scanf("%lf", &a2);
+    printf("b1 = ");
+    scanf("%lf", &b1);
+    printf("b2 = ");
+    scanf("%lf", &b2);
+    printf("c1 = ");
+    scanf("%lf", &c1);
+    printf("c2 = ");
+    scanf("%lf", &c2);
+
+    x = (b2 * c1 - b1 * c2) / (a1 * b2 - a2 * b1);
+    y = (a1 * c2 - a2 * c1) / (a1 * b2 - a2 * b1);
+
+    printf("x = %0.2lf, y = %0.2lf\n", x, y  );
+
+    */
+    /*
+     double a1, a2, b1, b2, c1, c2, d, x, y;
+
+    printf("a1 = ");
+    scanf("%lf", &a1);
+    printf("a2 = ");
+    scanf("%lf", &a2);
+    printf("b1 = ");
+    scanf("%lf", &b1);
+    printf("b2 = ");
+    scanf("%lf", &b2);
+    printf("c1 = ");
+    scanf("%lf", &c1);
+    printf("c2 = ");
+    scanf("%lf", &c2);
+
+    d = a1 * b2 - a2 * b1;
+
+    if((int) d == 0) {
+        printf("Value of X and y can not be determined. \n");
+    } else {
+        x = (b2 * c1 - b1 * c2) / d;
+        y = (a1 * c2 - a2 * c1);
+    }
+
+     printf("x = %0.2lf, y = %0.2lf\n", x, y  );
+
+     */
+
+    double loan_amount, interest_rat, number_of_years, total_amount,  monthly_amount;
+
+    printf("Enter the loan amount: ");
+    scanf("%lf", &loan_amount);
+    printf("Enter the interest rate: ");
+    scanf("%lf", &interest_rat);
+     printf("Number of years: ");
+    scanf("%lf", &number_of_years);
+
+    total_amount = loan_amount + loan_amount * interest_rat / 100.00;
+    monthly_amount = total_amount / (number_of_years * 12);
+
+    printf("Total amount: %0.21f\n", total_amount);
+    printf("Monthly amount: %0.21f\n", monthly_amount);
 
    return 0;
 }
